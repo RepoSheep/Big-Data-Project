9f5458a2a9e960049353d1d9b511392af1a8aea1
Alex Chen <boweichen@live.com>
2023-03-05 23:55:09 +0800

    include random sleep function

--- web scraping.py
+++ web scraping.py
@@ -9,9 +9,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 import sys
-# import logging
+import random
+import logging
 
-# set up logging
+"""
+# Set up logging
+
+logging.basicConfig(filename='run.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+"""
+
+
+# Create a random sleep function to avoid being blocked by the website.
+def random_sleep():
+    sleep(round(random.random() * 3, 2))
+
+
+
+
 
 
 # Print out the libraies version
@@ -20,6 +36,7 @@ print(f"python version: {sys.version.split(' ')[0]}")
 
 # Create empty DataFrame to store the data.
 columns = [
+    'job_post_id',
     'title',
     'company_name',
     'location',
@@ -46,7 +63,7 @@ count = -1
 for page in range(2): 
     driver.get(f'https://www.mycareersfuture.sg/search?search={job_keyword}&sortBy=new_posting_date&page={page}')
     assert 'MyCareersFuture' in driver.title #check if the page is loaded with MyCareersFuture
-    sleep(6)
+    sleep(1)
     #loop for first 2 jobs entries only
     for i in range(2):
         print("\n current page: {} \n job number:{}".format(page, i))
@@ -61,7 +78,7 @@ for page in range(2):
         # Click in job details.
         # loc_elem = "//div[@class='card-list']/div[{}]/div/a/div[1]/div/section/div[2]/div[2]/section/p[1]".format(i+1)
         driver.find_element("xpath",title_elem).click()
-        sleep(5)
+        random_sleep()
 
         # Extract other detail job information.
         employment_type = driver.find_element(By.ID, 'employment_type').text
@@ -72,6 +89,14 @@ for page in range(2):
         job_description = driver.find_element(By.ID, 'job_description').text
 
         try:
+            job_post_id = driver.find_element(By.XPATH, "//*[@id='job-details']/div[1]/div[3]/div/span").text
+            print(job_post_id)
+        except:
+            print("No job post id found")
+
+
+        try:
+            #get the salary range
             salary_min = driver.find_element(By.XPATH, "//div[@class='lh-solid']/span[1]").text
             salary_max = driver.find_element(By.XPATH, "//div[@class='lh-solid']/span[2]").text[2:]
         except:
@@ -90,13 +115,15 @@ for page in range(2):
         print(salary_max)
         # print(job_description)      
         
+        #increase the count and add the data to the dataframe, where first row is 0, since count starts from -1
         count += 1
-        jobs.loc[count] = [title, company_name, location, employment_type, level, category, min_experience,salary_min, salary_max, job_description]
+        jobs.loc[count] = [job_post_id,title, company_name, location, employment_type, level, category, min_experience,salary_min, salary_max, job_description]
         print(jobs)
+        # Go back to the search page.
         driver.back()
-        sleep(3)
+        random_sleep()
 
 
 # Save the data to csv file.
-jobs.to_csv(f'{job_keyword}.csv', index=False)
+jobs.to_csv(f'{job_keyword}.csv', index=False, quotechar='"',chunksize = 1000, encoding='utf-8')
 driver.close()
